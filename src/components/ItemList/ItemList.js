eed14497d378b52091d46543946fac81df6e63ec
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-10-04 16:03:24 -0300

    agregando productos

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles.css"
+
+function ItemList() {
+    return (
+        <div className='ItemList'>
+            <h1>Esto es el ItemList</h1>
+        </div>
+    )
+}
+
+export default ItemList
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ItemList from '../ItemList/ItemList'
 import "./styles.css"
 
 function ItemListContainer() {
     return (
         <div className='ItemListContainer'>
          <h1>Esto es el Item List Container</h1>
+         <ItemList/>
         </div>
     )
 }
