8473a1dcd53689a9ca19e4e642f545c68d818c3e
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-10-05 18:56:59 -0300

    carrito como componente

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CartWidget() {
+    return (
+        <a onClick ={handleClick}href='/#h'> <img src={imagenCarrito} alt="Carrito" className='imagenCarrito'></img></a>
+    )
+}
+
+export default CartWidget
