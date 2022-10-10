29524e23352df55e9ccef56fb8891e37c6d7d189
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-10-10 20:18:31 -0300

    Funciona catalogo y detalles de cada producto

--- src/components/ItemDetailsContainer/ItemDetailsContainer.js
+++ src/components/ItemDetailsContainer/ItemDetailsContainer.js
@@ -5,32 +5,30 @@ import getFetch from '../Data/data';
 import Navbar from '../NavBar/Navbar'
 
 function ItemDetailsContainer() {
-    let itemId = useParams()
+    let {detallesId} = useParams()
     const[data,setData]=useState([])
 
-    console.log(itemId)
+    console.log(detallesId)
 
   useEffect(()=> {
     getFetch
-    .then((resp)=>setData(resp))
+    .then((resp) => setData(resp.find((item) => item.id===Number(detallesId))))
     .catch(err=>console.log(err))
-  },[])
+  },[detallesId])
 
   console.log(data)
-  let producto = data.filter(data => data.id=== itemId)
-
+  
   //let filteredData = data.filter (data => data.tipo === tipoDeFiltro)
     return (
         <div>
             <Navbar/>
             <h3> Esto es el Item Details Container</h3>
-            <p>{producto.nombre}</p>
-            <p>{producto.precio}</p>
-            <img src={producto.imagen} alt="Imagen del producto"></img>
-            <p>{producto.stock}</p>
-            <p>{producto.detalles}</p>
-            <p>{producto.color1}</p>
-            <p>{producto.color2}</p>
+            <p>{data.precio}</p>
+            <img src={data.imagen} alt="Imagen del producto"></img>
+            <p>{data.stock}</p>
+            <p>{data.detalles}</p>
+            <p>{data.color1}</p>
+            <p>{data.color2}</p>
         </div>
     )
 }
