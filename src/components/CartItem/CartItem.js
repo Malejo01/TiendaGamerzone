70536ca8fc3e46411b64462d011f9e90cf21325a
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-10-25 12:05:10 -0300

    viendo por que no se guardan los datos en el carrito

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -7,16 +7,14 @@ function CartItem({ item, deleteCartById }) {
     return (
         <article className="cart-item-card">
             <div className="cart-item__delete" onClick={()=>deleteCartById(item.id)}>
-                
             </div>
             <div className="cart-item__img">
-                <img src={item.thumbnail} alt="Imagen"/>
+                <img src={item.imagen} alt="Imagen"/>
             </div>
-            <h2 className="cart-item__name">{item.name}</h2>
-            <span className="cart-item__price">${item.price}</span>
-            <span className="cart-item__qty"><strong>Cantidad:</strong> {item.qty}</span>
-            <span className="cart-item__qty"><strong>Total:</strong> ${item.qty * item.price}</span>
-            
+            <h2 className="cart-item__name">{item.nombre}</h2>
+            <span className="cart-item__price">${item.precio}</span>
+            <span className="cart-item__qty"><strong>Cantidad:</strong> {item.quantity}</span>
+            <span className="cart-item__qty"><strong>Total:</strong> ${item.quantity * item.precio}</span>
         </article>
     )
 }
